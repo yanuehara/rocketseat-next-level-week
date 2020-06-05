836bf4421641426c5b4b4e913d02e0107801fb24
Yan Uehara <yanuehara@hotmail.com>
2020-06-05 18:30:01 -0300

    Adds filtered index to points endpoint
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -1,7 +1,27 @@
-import {Request, Response, request } from 'express';
+import {Request, Response} from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+    async index(req: Request, res: Response) {
+        const {city, uf, items} = req.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(
+                item => Number(item.trim())
+            );
+
+        const points = await knex("points")
+            .join("point_items", "points.id", "=", "point_items.point_id")
+            .whereIn("point_items.item_id", parsedItems)
+            .where("city", String(city))
+            .where("uf", String(uf))
+            .distinct()
+            .select("points.*");
+
+        return res.json(points);
+    }
+
     async create(req: Request, res: Response) {
         const {
             name,

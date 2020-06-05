5f4591a28a7a5c20c705b3f19826de3758ee84b5
Yan Uehara <yanuehara@hotmail.com>
2020-06-05 17:48:00 -0300

    Adds point POST route
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -17,4 +17,45 @@ routes.get('/items', async (req, res) => {
     return res.json(serializedItems);
 });
 
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = req.body;
+
+    const trx = await knex.transaction();
+
+    const ids = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const point_id = ids[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        }
+    });
+
+    await trx('point_items').insert(pointItems);
+
+    trx.commit();
+
+    return res.json({success: true});
+});
+
 export default routes;

41a8e1c1d12d5881a505dee470bd62409a6ca406
Yan Uehara <yanuehara@hotmail.com>
2020-06-05 17:37:14 -0300

    Corrects items listing
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,6 +8,7 @@ routes.get('/items', async (req, res) => {
 
     const serializedItems = items.map( item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         }

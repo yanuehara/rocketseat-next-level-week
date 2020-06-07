a901200bcda86dd765f0950dd388151f1941c3f7
Yan Uehara <yanuehara@hotmail.com>
2020-06-06 21:06:49 -0300

    Adds maps to CreatePoint form
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -65,6 +66,15 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço</span>
                     </legend>
+
+                    <Map center={ [-25.4672982, -49.2333925] } zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={ [-25.4672982, -49.2333925] } />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">

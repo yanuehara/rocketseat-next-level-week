755888225d7fafdb9dddbb5d7be6c9d968d662bb
Yan Uehara <yanuehara@hotmail.com>
2020-06-07 02:27:02 -0300

    Adds Points view skeleton
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,9 +1,96 @@
 import React from 'react';
-import { View, StyleSheet} from 'react-native';
+import { View, Text, Image, ScrollView, StyleSheet} from 'react-native';
 import Constants from 'expo-constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {Feather as Icon} from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 
 const Points = () => {
-    return <View />
+    const navigation = useNavigation();
+
+    function handleNavigateBack(){
+        navigation.goBack();
+    }
+
+    function handleNavigateToDetail(){
+        navigation.navigate('Detail');
+    }
+    
+    return (
+        <>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={handleNavigateBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79"/>
+                </TouchableOpacity>
+
+                <Text style={styles.title}>Bem vindo!</Text>
+                <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
+
+                <View style={styles.mapContainer}>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={
+                            {
+                                latitude: -25.4481565,
+                                longitude: -49.240216,
+                                longitudeDelta: 0.014,
+                                latitudeDelta: 0.014
+                            }
+                        }
+                    >
+                    <Marker
+                        style={styles.mapMarker}
+                        coordinate={{latitude: -25.4481565, longitude: -49.240216}}
+                        onPress={handleNavigateToDetail}
+                    >
+                        <View style={styles.mapMarkerContainer}>
+                            <Image 
+                                style={styles.mapMarkerImage}
+                                source={{uri: 'https://images.unsplash.com/photo-1533900298318-6b8da08a523e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'}}
+                            />
+                            <Text style={styles.mapMarkerTitle}>Localização</Text>
+                        </View>
+                    </Marker>
+                    </MapView>
+                </View>
+            </View>
+        
+            <View style={styles.itemsContainer}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{ paddingHorizontal: 20 }}
+                >
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.item} onPress={() => {}}>
+                        <SvgUri width={42} height={42} uri="http://192.168.1.28:3333/uploads/lampadas.svg"/>
+                        <Text style={styles.itemTitle}>Lâmpadas</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({

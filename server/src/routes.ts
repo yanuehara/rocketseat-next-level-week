7621f183a24fc9d076ed6e3469fb5aee391c56be
Yan Uehara <yanuehara@hotmail.com>
2020-06-07 23:25:41 -0300

    Adds data validation on point creation
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
@@ -12,7 +13,25 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            uf: Joi.string().required().max(2),
+            city: Joi.string().required(),
+            items: Joi.string().required()
+        })
+    },{abortEarly: false}),
+    pointsController.create
+    );
+
+
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
